0afb7deab9069d4bfcfde8558d74edc0b3a4369e
lehmaynardi <132676542+lehmaynardi@users.noreply.github.com>
2023-09-01 09:33:20 -0300

    Add files via upload

---
+++ 2e-3trim-nutricao-main/js/principal.js
@@ -0,0 +1,11 @@
+//atribui o valor da tag h1 para a variável título
+let titulo = document.querySelector("h1").textContent;
+//mostra no console do navegador a variável título
+console.log(titulo);
+//modifica o valor o titulo para a frase "Fica grandão"
+titulo.textContent = "Fica grandão birllll";
+
+//---------aqui vamo mexer na tebal e imc----
+
+let paciente = document.querySelector("#primeiro-paciente")
+console.log (paciente);
